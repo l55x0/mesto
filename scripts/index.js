42221b1aceaa863c686981a1f98996344d04f005
Artyom Lodochnikov <lodkin55@yandex.ru>
2020-10-26 23:54:32 +0500

    feat: Создать логику заполнения формы и отображения на странице

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 // Выбираем элементы Popup
 const popup = document.querySelector('.popup');
 const popupContent = popup.querySelector('.popup__container'); // Выбираем контейнер popup
-const popupCloseButton = popup.querySelector('.popup__close'); // Выбираем элементы контейнера popup
+const popupCloseButton = popup.querySelector('.popup__close'); // Выбираем элементы контейнера popup и кнопку закрытия
 const popupTitle = popup.querySelector('.popup__title');
-const popupForm = popup.querySelector('.popup__input-container'); // Выбираем форму
+const popupForm = popup.querySelector('.popup__input-container'); // Выбираем форму и элементы формы
 const popupNameField = popupForm.querySelector('.popup__input_type_author');
-const nameField = popupForm.querySelector('.popup__input_type_status');
+const popupStatusField = popupForm.querySelector('.popup__input_type_status');
+const popupSubmitButton = popup.querySelector('.button_type_submit'); // Выбираем кнопу отправить 
 
 // Подключаем элементы блока Profile
 const profileEditButton = document.querySelector('.button_type_edit'); // Выбираем кнопку редактирования
@@ -16,12 +17,33 @@ const profileSubtitle = document.querySelector('.profile__status');
 function showPopup() {
   popup.classList.add('popup_opened');
   popup.removeEventListener('click', showPopup);
+
+  // Привязываем стартовое значение в Popap поля
+  popupNameField.value = profileTitle.textContent;
+  popupStatusField.value = profileSubtitle.textContent;
 }
 
 // функция закрытия Popup
 function closePopup() {
   popup.classList.remove('popup_opened')
+
+  // Привязываем значение полей после закрытия Popap
+  popupNameField.value = profileTitle.textContent;
+  popupStatusField.value = profileSubtitle.textContent;
 }
 
-profileEditButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', showPopup); // Отслеживаем событие клика кнопки "редактировать" 
+popupCloseButton.addEventListener('click', closePopup); // Отслеживаем событие клика кнопки "закрыть" 
+
+// Обработчик «отправки» формы
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  profileTitle.textContent = popupNameField.value;
+  profileSubtitle.textContent = popupStatusField.value;
+
+  closePopup();
+}
+
+// Отслеживаем событие клика кнопки "отправить" 
+popupSubmitButton.addEventListener('click', formSubmitHandler);
