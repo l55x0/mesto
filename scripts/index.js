bdc634f688644dfe55e8f48dc380f5c494bd7e55
Artyom Lodochnikov <lodkin55@yandex.ru>
2020-10-27 14:13:16 +0500

    fix: Исправить недочеты Popup Form

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 // Выбираем элементы Popup
 const popup = document.querySelector('.popup');
 const popupContent = popup.querySelector('.popup__container'); // Выбираем контейнер popup
-const popupCloseButton = popup.querySelector('.popup__button-close'); // Выбираем элементы контейнера popup и кнопку закрытия
-const popupTitle = popup.querySelector('.popup__title');
-const popupForm = popup.querySelector('.popup__input-container'); // Выбираем форму и элементы формы
+const popupCloseButton = popupContent.querySelector('.popup__button-close'); // Выбираем элементы контейнера popup и кнопку закрытия
+const popupTitle = popupContent.querySelector('.popup__title');
+const popupForm = popupContent.querySelector('.popup__form'); // Выбираем форму и элементы формы
 const popupNameField = popupForm.querySelector('.popup__input_type_author');
 const popupStatusField = popupForm.querySelector('.popup__input_type_status');
-const popupSubmitButton = popup.querySelector('.popup__button-submit'); // Выбираем кнопу отправить 
+const popupSubmitButton = popupForm.querySelector('.popup__button-submit'); // Выбираем кнопу отправить 
 
 // Выбираем элементы блока Profile
 const profileEditButton = document.querySelector('.profile__button-edit'); // Выбираем кнопку редактирования
@@ -32,6 +32,7 @@ function closePopup() {
 function formSubmitHandler(event) {
   event.preventDefault();
 
+  // Отправляем значения строк формы на страницу
   profileTitle.textContent = popupNameField.value;
   profileSubtitle.textContent = popupStatusField.value;
 
@@ -40,4 +41,4 @@ function formSubmitHandler(event) {
 
 profileEditButton.addEventListener('click', showPopup); // Отслеживаем событие клика кнопки "редактировать" 
 popupCloseButton.addEventListener('click', closePopup); // Отслеживаем событие клика кнопки "закрыть" 
-popupSubmitButton.addEventListener('submit', formSubmitHandler); // Отслеживаем событие клика кнопки "отправить" 
+popupForm.addEventListener('submit', formSubmitHandler); // Отслеживаем событие клика кнопки "отправить"
