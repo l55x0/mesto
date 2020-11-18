0a13113a88e3087ab1c8a9057d3efb8d8ecedb29
Artyom Lodochnikov <lodkin55@yandex.ru>
2020-11-18 10:32:55 +0500

    fix: Исправить все ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -53,12 +53,6 @@ const placesList = document.querySelector('.places');
 // Функуция открытия Popup
 function showPopup(popup) {
   popup.classList.add('popup_opened');
-
-  if (popup === popupProfile) {
-    popupNameField.value = profileTitle.textContent;
-    popupStatusField.value = profileSubtitle.textContent;
-  };
-
   popup.removeEventListener('click', showPopup);
 };
 
@@ -67,66 +61,73 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
 
-function addPlace(place) {
-  const placesElement = document.querySelector(".places-template").content.cloneNode(true);
+//Функция создания карточки 
+function createCard(name, link) {
+  const element = document.querySelector(".places-template").content.cloneNode(true);
+
+  // находим элементы в DOM
+  const elementTitle = element.querySelector(".place__title");
+  const elementImage = element.querySelector(".place__image");
+  const elementImageAlt = element.querySelector(".place__image");
 
   // Подставляем значения из массива в шаблон HTML
-  placesElement.querySelector(".place__title").textContent = place.name;
-  placesElement.querySelector(".place__image").src = place.link;
-  placesElement.querySelector(".place__image").alt = 'Фотография местности ' + place.name;
+  elementTitle.textContent = name;
+  elementImage.src = link;
+  elementImageAlt.alt = 'Фотография местности ' + name;
 
   // Отслеживаем событие клика кнопки Удаление
-  placesElement.querySelector(".place__button-remove").addEventListener("click", evt => {
-    const place = evt.target.closest(".place");
-
-    if (place) {
-      place.remove();
-    }
+  element.querySelector(".place__button-remove").addEventListener("click", evt => {
+    evt.target.closest(".place").remove();
   });
 
   // Отслеживаем событие клика кнопки Лайк
-  placesElement.querySelector(".place__button-like").addEventListener('click', evt => {
+  element.querySelector(".place__button-like").addEventListener('click', evt => {
     evt.target.classList.toggle("place__button-like_active");
   });
 
   // Отслеживаем событие клика на картинку
-  placesElement.querySelector(".place__image").addEventListener('click', evt => {
+  element.querySelector(".place__image").addEventListener('click', evt => {
 
     document.querySelector('.popup__image').src = evt.target.src;
-    document.querySelector('.popup__caption').textContent = place.name;
+    document.querySelector('.popup__caption').textContent = name;
 
     showPopup(popupImage);
   });
 
-  placesList.prepend(placesElement);
+  return element; //возвращается созданная карточка 
 }
 
-initialCards.forEach(addPlace); //Выводим данные из массива на страницу
+// Функция добавления карточки в контейнер
+function addCard(container, cardElement) {
+  container.prepend(cardElement);
+}
 
+// Перебор элементов массива с функцией addCard
+initialCards.forEach(item => {
+  addCard(placesList, createCard(item.name, item.link));
+});
+
+// Перебор всей попапов с целью повешать обработчик на все кнопки закрытия
 popups.forEach(elem => {
   elem.querySelector('.popup__button-close').addEventListener('click', () => {
     closePopup(elem);
   });
-  //Обходим массив попапов и находим нужный
-  //и отслеживаем нажатие на кнопку закрытия
 });
 
+// Отслеживаем событие отправки формы "Новой карточки"
 popupFormAdd.addEventListener("submit", evt => {
   evt.preventDefault()
 
-  const newPlace = []; // Массив для отправки в функцию AddPlace
+  const newNameCard = popupFormAdd.querySelector('.popup__input_type_place-name').value; // Значения полей формы 
+  const newLinkCard = popupFormAdd.querySelector('.popup__input_type_photo').value;
 
-  newPlace.name = popupFormAdd.querySelector('.popup__input_type_place-name').value; // Значения полей формы 
-  newPlace.link = popupFormAdd.querySelector('.popup__input_type_photo').value;
+  addCard(placesList, createCard(newNameCard, newLinkCard)); // Добавляем новую карточку
 
-  addPlace(newPlace);
-
-  closePopup(popupAddCard);
-  popupFormAdd.reset();
-  // Отслеживаем событие отправки формы Новой карточки
-  // и отправляем полученые значения в функцию добавления карточки
+  closePopup(popupAddCard); // закрываем форму
+  popupFormAdd.reset(); // сбрасываем значения формы
 });
 
+// Отслеживаем событие отправки формы "Редактирования Profile" и отправляем полученые значения 
 popupFormEdit.addEventListener("submit", evt => {
   evt.preventDefault()
 
@@ -134,17 +135,18 @@ popupFormEdit.addEventListener("submit", evt => {
   profileSubtitle.textContent = popupStatusField.value;
 
   closePopup(popupProfile);
-  // Отслеживаем событие отправки формы Редактирования Profile
-  // и отправляем полученые значения 
 });
 
+// Отслеживаем событие клика кнопки "редактировать" 
 profileEditButton.addEventListener('click', () => {
-  showPopup(popupProfile);
-}); // Отслеживаем событие клика кнопки "редактировать" 
+  popupNameField.value = profileTitle.textContent;
+  popupStatusField.value = profileSubtitle.textContent;
 
+  showPopup(popupProfile);
+});
+
+// Отслеживаем событие клика кнопки "добавить карточку" 
 profileAddButton.addEventListener('click', () => {
   showPopup(popupAddCard);
-}); // Отслеживаем событие клика кнопки "добавить карточку" 
-
-
+});
 
