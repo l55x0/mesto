a6b62d497e880bcbe1e9c10ce4b151e3e708f05b
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-01-20 13:09:55 +0500

    fix: Баг со слушателями попапа удаления карточек

--- src/components/PopupWithRemove.js
+++ src/components/PopupWithRemove.js
@@ -7,6 +7,11 @@ export default class PopupWithRemove extends Popup {
     this._handleRemoveClick = handleRemoveClick
   }
 
+  close() {
+    super.close()
+    this._popupButton.removeEventListener('click', this._handleRemoveClick);
+  }
+
   setEventListeners() {
     super.setEventListeners();
     this._popupButton.addEventListener('click', this._handleRemoveClick);
